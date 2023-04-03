ea814f48520362a15e6395335ed0d3649e334502
Mark Aldrich <mark@mwaldrich.io>
2023-04-03 17:59:39 -0400

    summarize.py skeleton uploaded

---
+++ summarize.py
@@ -0,0 +1,7 @@
+# Summarizes the article.
+# The argument `text` is a String, containing the text of the article.
+# This function returns a String, containing the ML-generated summary of the article.
+def summarize(text):
+    # TODO: Li implement
+    return "summary not yet implemented"
+
